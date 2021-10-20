c55637785f21f2164bedf6e6279097504d725e6a
AlexRuppert <alexander.ruppert@rwth-aachen.de>
2021-10-20 18:23:53 +0200

    config & matomo fix

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -13,27 +13,21 @@ export function shuffle(array) {
   return arrayClone
 }
 
-export async function logPageView() {
-  try {
-    const url = new URL('https://www.xenium.com/piwik/piwik.php')
-    const now = new Date()
-    const params = {
-      action_name: `[DATA SCIENCE] ${document.title}`,
-      idsite: '1',
-      rec: '1',
-      r: String(Math.random()).slice(2, 8),
-      h: now.getHours().toString(),
-      m: now.getMinutes().toString(),
-      s: now.getSeconds().toString(),
-      url: window.location.origin + window.location.pathname,
-      send_image: '1',
-    }
-    url.search = new URLSearchParams(params).toString()
-    return await fetch(url.toString(), {
-      headers: { 'sec-fetch-mode': 'no-cors' },
-    })
-  } catch (ex) {
-    console.error(ex)
+export function logPageView() {
+  const img = document.createElement('img')
+  const url = new URL('https://www.xenium.com/piwik/piwik.php')
+  const now = new Date()
+  const params = {
+    action_name: `[DATA SCIENCE] ${document.title}`,
+    idsite: '1',
+    rec: '1',
+    r: String(Math.random()).slice(2, 8),
+    h: now.getHours().toString(),
+    m: now.getMinutes().toString(),
+    s: now.getSeconds().toString(),
+    url: window.location.origin + window.location.pathname,
+    send_image: '1',
   }
-  return null
+  url.search = new URLSearchParams(params).toString()
+  img.src = url.toString()
 }
